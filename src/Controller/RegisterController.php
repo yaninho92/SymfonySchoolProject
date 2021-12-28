cfd05ba2ae70e26cc02f1acc4e55441f3f73791b
Yani Moucer <yani9245@gmail.com>
2021-12-28 12:38:02 +0100

    finsh Register add Connexion

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,25 +4,43 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegisterController extends AbstractController
 {
     /**
-     * @Route("/inscription", name="register", methods={"GET"})
+     * @Route("/inscription", name="register", methods={"GET|POST"})
      * @param Request $request
      * @return Response
      */
-    public function register(Request $request): Response
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
 
         $form = $this->createForm(RegisterType::class, $user);
 
         $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $user = $form->getData();
+
+            $user->setCreatedAt(new DateTime());
+
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+
+            $user->setRoles(['ROLE_USER']);
+            
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
 
         return $this->render('register/register.html.twig', [
             'form' => $form->createView()
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -5,20 +5,74 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
+            ->add('email', EmailType::class,[
+                'required' => 'true',
+                'attr' => [
+                    'placeholder' => 'Entrez votre e-mail'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Ce champs ne peut être vide, veuillez entrez un email'
+                    ]) 
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'mot de passe',
+                'attr' => [
+                    'placeholder' => 'Entrez un mot de passe'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Ce champs ne peut être vide, veuillez entrez un mot de passe'
+                    ]) 
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre prénom'
+                ],
+                'constraints' => [
+                    new NotBlank(['message' => 'Ce champs ne peut être vide, veuillez entrez votre prénom']),
+                    new Length([
+                        'min' => 2,
+                        'max' => 20,
+                        'minMessage' => 'Votre prénom est trop court, il doit contenir au minimum {{ limit }} caractères'
+                    ])
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre Nom'
+                ],
+                'constraints' => [
+                    new NotBlank(['message' => 'Ce champs ne peut être vide, veuillez entrez votre prénom']),
+                    new Length([
+                        'min' => 2,
+                        'max' => 20,
+                        'minMessage' => 'Votre prénom est trop court, il doit contenir au minimum {{ limit }} caractères'
+                    ])
+                ]
+            ])
             ->add('submit', SubmitType::class, [
-                'label' => 'Valider'
+                'label' => 'Valider',
+                'attr' => [
+                    'class' => 'd-block col-3 mx-auto btn btn-success'
+                ]
             ])
         ;
     }
